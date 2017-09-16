f995169b1fb67c35949764c502dae61a3ec33a44
Rails Udemy Course <mizdac17@gmail.com>
2017-09-16 06:36:38 -0700

    Fix create, add flass error messages and show new created article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,21 @@ class ArticlesController < ApplicationController
   end
   def create
     #render plain: params[:article].inspect
-    @article =Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfuly created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    #@article.save
+    #redirect_to article_path(@article)
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   private 
     def article_params
       params.require(:article).permit(:title, :description)
